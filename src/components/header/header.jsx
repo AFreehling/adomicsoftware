04254e45935556749bbbe1b0a76d7f84f0bd19a6
AFreehling <adam@Glados.localdomain>
2020-01-25 16:24:44 -0500

    messing around

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,7 +11,6 @@ const Header = ({ siteTitle }) => (
   >
     <div
       style={{
-        margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
@@ -27,6 +26,8 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <div style={{ marginTop: `1.45rem`, textAlign: `left`, float: `left`}}>About</div>
+      <div style={{ marginTop: `1.45rem`, textAlign: `left` }}>Project</div>
     </div>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./header/header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -28,7 +28,6 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
